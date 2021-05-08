40144d1739bf1b2bb40ad4a25c0f2e765b626476
Sergey Karlinsky <4923920@gmail.com>
2021-05-08 13:34:05 +0300

    fixed button bug in mobile version

--- app/js/components/Animation.js
+++ app/js/components/Animation.js
@@ -8,6 +8,8 @@ export default class Animation {
 		const rect = el.getBoundingClientRect();
 		let scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
 		let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        console.log(rect.top + scrollTop);
+
         return {
 			top: rect.top + scrollTop, left: rect.left + scrollLeft 
 		}
@@ -17,31 +19,32 @@ export default class Animation {
     
         for (let i=0; i < this.animItems.length; i++) {
             const animItem = this.animItems[i];
+            console.log(this.animItems[i]);
             const animItemHeight = animItem.offsetHeight;
             const animItemOffset = this._offset(animItem).top;
-            console.log( animItemHeight, animItemOffset, window.innerHeight, pageYOffset)
             const animStart = 4;
 
             let animItemPoint = window.innerHeight - animItemHeight / animStart;
             if (animItemHeight > window.innerHeight) {
                 animItemPoint = window.innerHeight - window.innerHeight / animStart;
-                console.log('work animItemPoint');
-                console.log(animItemPoint);
-
+                console.log('if1');
             }
-            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight) ) {
-                console.log('pageYOffset ='+pageYOffset, 'animItemOffset ='+animItemOffset, 'animItemPoint='+animItemPoint);   
-                
-                
-                animItem.classList.add('_active');
-
-
+            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight) ) {         
+                    animItem.classList.add('_active');
+                    console.log('if2');
             } else {
+                console.log('else');
+                console.log('pageYoffset is '+pageYOffset);
+                console.log('animItemOffset is '+animItemOffset);
+                console.log('animItemPoint is '+animItemPoint);
+                console.log('animItemHeight is '+animItemHeight);
+                console.log('first if equal '+(animItemOffset - animItemPoint));
+                console.log('second if equal '+(animItemOffset + animItemHeight));
                 if (!animItem.classList.contains('_anim-no-hide')) {
+                    console.log('if3');
                     animItem.classList.remove('_active');
                 }
             }
-
         }
 
     }
@@ -50,7 +53,6 @@ export default class Animation {
         if(this.animItems.length > 0) {
             setTimeout(() => {
                 this._animOnScroll(); 
-                console.log('work animation')
             }, 300);
         }
     }
